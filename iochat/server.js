e8970468cb3531cd12225c6d98b51d0fe67587a7
杨振宇 <andy@yang.local>
2017-05-10 11:14:35 -0400

    Add comments to iochat

--- iochat/server.js
+++ iochat/server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-
 var io = require('socket.io').listen(server);
 
-users = [];
-connections = [];
+users = []; // hold all users
+connections = []; // hold all socket connections
 server.listen(process.env.PORT || 3000);
 console.log('Server running ..');
 
@@ -14,30 +13,30 @@ app.get('/', function(req, res) {
 });
 
 io.sockets.on('connection', function(socket) {
-  connections.push(socket);
+  connections.push(socket); // push in socket when user connects
   console.log('Connected: %s sockets connected', connections.length);
 
   // Disconnect
   socket.on('disconnect', function(data) {
-    // if(!socket.username) return;
+    // remove user when disconnected
     users.splice(users.indexOf(socket.username), 1);
     updateUsernames();
+    // also remove the socket connection
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected: %s sockets connected', connections.length);
   });
 
-
+  // recieve cleint messages
   socket.on('send message', function(data) {
-    console.log(data);
-    io.sockets.emit('new message', {msg: data, user: socket.username });
+    // console.log(data);
+    io.sockets.emit('new message', { msg: data, user: socket.username });
   });
 
   // new user
-  socket.on('new user', function(data, callback) {
-    callback(true);
-    console.log("user data: " + data);
-    socket.username = data;
-    users.push(socket.username);
+  socket.on('new user', function(user, callback) {
+    callback(true); // return true to client
+    socket.username = user; // add user name info to socket
+    users.push(user);
     updateUsernames();
   });
 
